8aa285cc552291f10ee13328f28479d916e66e3e
OKarpacz <156137609+OKarpacz@users.noreply.github.com>
2024-01-10 09:01:03 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -349,7 +349,7 @@ int main () {
 
 	init_pair(0, COLOR_WHITE, COLOR_BLACK);
 	init_pair(1, COLOR_GREEN, COLOR_BLACK);
-	init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(2, COLOR_RED, COLOR_BLACK);
 	init_pair(3, COLOR_BLUE, COLOR_BLACK);
 	init_pair(4, COLOR_BLACK, COLOR_CYAN);
 
